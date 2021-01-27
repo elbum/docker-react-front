76c064a00807f7ca2d3d4fc1ac065f82ce84251b
elbum <elbumlee@gmail.com>
2021-01-27 19:30:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 안녕하세요.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          안녕하세요 Learn React v4
         </a>
       </header>
     </div>
